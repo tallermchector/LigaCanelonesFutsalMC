b9ca92ae362f8b212666a226f3845dcec133d604
Hector Larrea <taller.mcmoto@gmail.com>
2025-08-31 23:14:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -7,24 +7,24 @@ export const socialLinks: SocialLink[] = [
     name: 'Instagram',
     url: 'https://www.instagram.com/ligacanariadefutsaloficial/',
     icon: InstagramIcon,
-    imageUrl: 'https://picsum.photos/1920/1080?random=1',
+    imageUrl: 'https://images.unsplash.com/photo-1571182873726-f1ba0cdf6bde?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHxzb2NpYWwlMjBtZWRpYSUyMGFic3RyYWN0fGVufDB8fHx8MTc1NjY4MjA2M3ww&ixlib=rb-4.1.0&q=80&w=1080',
   },
   {
     name: 'Facebook',
     url: 'https://www.facebook.com/Ligacanariadefutsal',
     icon: FacebookIcon,
-    imageUrl: 'https://picsum.photos/1920/1080?random=2',
+    imageUrl: 'https://images.unsplash.com/photo-1698332328523-a450d7d29d95?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxzb2NpYWwlMjBtZWRpYSUyMGFic3RyYWN0fGVufDB8fHx8MTc1NjY4MjA2M3ww&ixlib=rb-4.1.0&q=80&w=1080',
   },
   {
     name: 'YouTube',
     url: 'https://youtube.com/@ligacanariadefutsaltv',
     icon: YoutubeIcon,
-    imageUrl: 'https://picsum.photos/1920/1080?random=3',
+    imageUrl: 'https://images.unsplash.com/photo-1511171735792-048024585d63?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMHx8c29jaWFsJTIwbWVkaWElMjBhYnN0cmFjdHxlbnwwfHx8fDE3NTY2ODIwNjN8MA&ixlib=rb-4.1.0&q=80&w=1080',
   },
   {
     name: 'Sitio Web',
     url: 'https://ligacanelonesfutsalmc.netlify.app/',
     icon: GlobeIcon,
-    imageUrl: 'https://picsum.photos/1920/1080?random=4',
+    imageUrl: 'https://images.unsplash.com/photo-1634836863374-aa57a032eb82?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxzb2NpYWwlMjBtZWRpYSUyMGFic3RyYWN0fGVufDB8fHx8MTc1NjY4MjA2M3ww&ixlib=rb-4.1.0&q=80&w=1080',
   },
 ];
