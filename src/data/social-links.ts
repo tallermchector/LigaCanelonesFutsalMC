aeebb2f9af0df85513764a8a3d4bfe883320ff38
Hector Larrea <taller.mcmoto@gmail.com>
2025-08-31 22:17:19 +0000

    puedes crear aqui src/data un nuevo .ts que sea para guardar los contact

---
+++ src/data/social-links.ts
@@ -0,0 +1,20 @@
+
+export interface SocialLink {
+  name: string;
+  url: string;
+}
+
+export const socialLinks: SocialLink[] = [
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/ligacanariadefutsaloficial/',
+  },
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/Ligacanariadefutsal',
+  },
+  {
+    name: 'Sitio Web',
+    url: 'https://ligacanelonesfutsalmc.netlify.app/',
+  },
+];
