4fe49f5f47da68586fc6a8656a2e5b865d986316
Hector Larrea <taller.mcmoto@gmail.com>
2025-08-31 23:04:50 +0000

    Título Descriptivo (Prompt 2 de 4): Enriquecer Datos de Redes Sociales c

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -7,20 +7,24 @@ export const socialLinks: SocialLink[] = [
     name: 'Instagram',
     url: 'https://www.instagram.com/ligacanariadefutsaloficial/',
     icon: InstagramIcon,
+    imageUrl: 'https://picsum.photos/1920/1080?random=1',
   },
   {
     name: 'Facebook',
     url: 'https://www.facebook.com/Ligacanariadefutsal',
     icon: FacebookIcon,
+    imageUrl: 'https://picsum.photos/1920/1080?random=2',
   },
   {
     name: 'YouTube',
     url: 'https://youtube.com/@ligacanariadefutsaltv',
     icon: YoutubeIcon,
+    imageUrl: 'https://picsum.photos/1920/1080?random=3',
   },
   {
     name: 'Sitio Web',
     url: 'https://ligacanelonesfutsalmc.netlify.app/',
     icon: GlobeIcon,
+    imageUrl: 'https://picsum.photos/1920/1080?random=4',
   },
 ];
--- src/types/index.ts
+++ src/types/index.ts
@@ -94,4 +94,5 @@ export interface SocialLink {
   name: string;
   url: string;
   icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+  imageUrl: string;
 }
