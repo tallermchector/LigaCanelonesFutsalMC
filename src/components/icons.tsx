32006a10e9511c5ad4a6e0f14e9b0e788f78ff41
Hector Larrea <taller.mcmoto@gmail.com>
2025-08-31 23:57:50 +0000

    [{ 	"resource": "/home/user/studio/src/components/icons.tsx", 	"owner":

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,25 +1,26 @@
 
 import type { SVGProps } from "react";
-import Image from 'next/image';
+import Image, { type ImageProps } from 'next/image';
 import { Globe } from 'lucide-react';
 
+type CustomIconProps = Omit<ImageProps, 'src' | 'alt'>;
 
-export function FutsalBallIcon({ width = 24, height = 24, ...props }: SVGProps<SVGSVGElement>) {
-  return <Image src="/logofu.svg" alt="Futsal ball icon" width={Number(width)} height={Number(height)} {...props} />;
+export function FutsalBallIcon({ width = 24, height = 24, ...props }: CustomIconProps) {
+  return <Image src="/logofu.svg" alt="Futsal ball icon" width={width} height={height} {...props} />;
 }
 
-export const InstagramIcon = ({ width = 48, height = 48, ...props }: SVGProps<SVGSVGElement>) => (
-  <Image src="/icon/icon-instagram.svg" alt="Instagram icon" width={Number(width)} height={Number(height)} {...props} />
+export const InstagramIcon = ({ width = 48, height = 48, ...props }: CustomIconProps) => (
+  <Image src="/icon/icon-instagram.svg" alt="Instagram icon" width={width} height={height} {...props} />
 );
 
-export const FacebookIcon = ({ width = 48, height = 48, ...props }: SVGProps<SVGSVGElement>) => (
-  <Image src="/icon/icon-facebook.svg" alt="Facebook icon" width={Number(width)} height={Number(height)} {...props} />
+export const FacebookIcon = ({ width = 48, height = 48, ...props }: CustomIconProps) => (
+  <Image src="/icon/icon-facebook.svg" alt="Facebook icon" width={width} height={height} {...props} />
 );
 
 export const GlobeIcon = (props: SVGProps<SVGSVGElement>) => (
   <Globe {...props} />
 );
 
-export const YoutubeIcon = ({ width = 48, height = 48, ...props }: SVGProps<SVGSVGElement>) => (
-    <Image src="/icon/icon-youtube.svg" alt="YouTube icon" width={Number(width)} height={Number(height)} {...props} />
+export const YoutubeIcon = ({ width = 48, height = 48, ...props }: CustomIconProps) => (
+    <Image src="/icon/icon-youtube.svg" alt="YouTube icon" width={width} height={height} {...props} />
 );
