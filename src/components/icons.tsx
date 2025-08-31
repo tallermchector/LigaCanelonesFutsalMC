dc3fc1d62c8fa6b332f1a69387bf786a731e78cb
Hector Larrea <taller.mcmoto@gmail.com>
2025-08-31 23:55:15 +0000

    [{ 	"resource": "/home/user/studio/src/components/icons.tsx", 	"owner":

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,27 +1,25 @@
 
 import type { SVGProps } from "react";
 import Image from 'next/image';
-import { Instagram, Facebook, Globe, Youtube } from 'lucide-react';
+import { Globe } from 'lucide-react';
 
 
-export function FutsalBallIcon(props: SVGProps<SVGSVGElement>) {
-  // Return an Image component pointing to the new SVG
-  // The props like className can be passed to the Image component for styling
-  return <Image src="/logofu.svg" alt="Futsal ball icon" width={24} height={24} {...props} />;
+export function FutsalBallIcon({ width = 24, height = 24, ...props }: SVGProps<SVGSVGElement>) {
+  return <Image src="/logofu.svg" alt="Futsal ball icon" width={Number(width)} height={Number(height)} {...props} />;
 }
 
-export const InstagramIcon = (props: SVGProps<SVGSVGElement>) => (
-  <Image src="/icon/instagram.svg" alt="Instagram icon" width={48} height={48} {...props} />
+export const InstagramIcon = ({ width = 48, height = 48, ...props }: SVGProps<SVGSVGElement>) => (
+  <Image src="/icon/icon-instagram.svg" alt="Instagram icon" width={Number(width)} height={Number(height)} {...props} />
 );
 
-export const FacebookIcon = (props: SVGProps<SVGSVGElement>) => (
-  <Image src="/icon/facebook.svg" alt="Facebook icon" width={48} height={48} {...props} />
+export const FacebookIcon = ({ width = 48, height = 48, ...props }: SVGProps<SVGSVGElement>) => (
+  <Image src="/icon/icon-facebook.svg" alt="Facebook icon" width={Number(width)} height={Number(height)} {...props} />
 );
 
 export const GlobeIcon = (props: SVGProps<SVGSVGElement>) => (
   <Globe {...props} />
 );
 
-export const YoutubeIcon = (props: SVGProps<SVGSVGElement>) => (
-    <Image src="/icon/youtube.svg" alt="YouTube icon" width={48} height={48} {...props} />
+export const YoutubeIcon = ({ width = 48, height = 48, ...props }: SVGProps<SVGSVGElement>) => (
+    <Image src="/icon/icon-youtube.svg" alt="YouTube icon" width={Number(width)} height={Number(height)} {...props} />
 );
