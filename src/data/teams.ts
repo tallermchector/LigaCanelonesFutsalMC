cc22c75416e8d5ae40fb76f858f575bfae386e4f
Hector Larrea <taller.mcmoto@gmail.com>
2025-08-31 07:23:16 +0000

    puedes usar los  players src/data/players.ts para usarlos con cada equip

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,95 +1,92 @@
 
 import type { Team } from '@/types';
+import { players } from './players';
 
-export const futsalTeams: Team[] = [
+const baseTeams: Omit<Team, 'players'>[] = [
   {
     id: 1,
     name: "LOS QUITU",
     logoUrl: "/equipos/1.png",
-    players: []
   },
   {
     id: 10,
     name: "LA PAPA MADRE",
     logoUrl: "/equipos/10.png",
-    players: []
   },
   {
     id: 11,
     name: "WANDERERS",
     logoUrl: "/equipos/11.png",
-    players: []
   },
   {
     id: 12,
     name: "JCC 1930",
     logoUrl: "/equipos/12.png",
-    players: []
   },
   {
     id: 13,
     name: "STELLA",
     logoUrl: "/equipos/13.png",
-    players: []
   },
   {
     id: 14,
     name: "REAL CANELONES",
     logoUrl: "/equipos/14.png",
-    players: []
   },
   {
     id: 15,
     name: "DAC",
     logoUrl: "/equipos/15.png",
-    players: []
   },
   {
     id: 2,
     name: "VALDEARCOS",
     logoUrl: "/equipos/2.png",
-    players: []
   },
   {
     id: 3,
     name: "NEGRIAZUL",
     logoUrl: "/equipos/3.png",
-    players: []
   },
   {
     id: 4,
     name: "PEDRENSE",
     logoUrl: "/equipos/4.png",
-    players: []
   },
   {
     id: 5,
     name: "LA PIECITA",
     logoUrl: "/equipos/5.png",
-    players: []
   },
   {
     id: 6,
     name: "EL HACHA",
     logoUrl: "/equipos/6.png",
-    players: []
   },
   {
     id: 7,
     name: "MILANO",
     logoUrl: "/equipos/7.png",
-    players: []
   },
   {
     id: 8,
     name: "T. DE CAMPEONES",
     logoUrl: "/equipos/8.png",
-    players: []
   },
   {
     id: 9,
     name: "CAPINCHO",
     logoUrl: "/equipos/9.png",
-    players: []
   }
 ];
+
+
+// Distribute players to teams
+export const futsalTeams: Team[] = baseTeams.map((team, index) => {
+  const startIndex = index * 10;
+  const endIndex = startIndex + 10;
+  return {
+    ...team,
+    players: players.slice(startIndex, endIndex)
+  };
+});
