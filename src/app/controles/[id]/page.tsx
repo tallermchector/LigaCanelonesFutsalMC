6eef247e44f962e77cf8ce886196e64b78482329
Hector Larrea <taller.mcmoto@gmail.com>
2025-08-31 08:22:16 +0000

    ss

--- src/app/controles/[id]/page.tsx
+++ src/app/controles/[id]/page.tsx
@@ -101,14 +101,7 @@ export default function MatchControlPage() {
       <div className="flex min-h-screen flex-col bg-[hsl(var(--background))]">
         <Header />
         <main className="container mx-auto flex flex-grow flex-col items-center py-8 px-4">
-          <div className="w-full flex justify-start mb-4 max-w-6xl">
-            <Button variant="outline" asChild>
-              <Link href="/controles">
-                <ArrowLeft className="mr-2 h-4 w-4" />
-                Volver a Controles
-              </Link>
-            </Button>
-          </div>
+    
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 w-full max-w-6xl mb-4">
             <div className="lg:col-span-2">
                 <Scoreboard />
