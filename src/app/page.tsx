4114071468dda74e75ca4aa15ba20962285dbe56
Hector Larrea <taller.mcmoto@gmail.com>
2025-09-03 13:12:57 +0000

    2025-09-03T13:09:43Z [web] Error:  ⨯ [Error: ENOENT: no such file or dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
 import { Footer } from '@/components/layout/footer';
 import { LiveMatchesBanner } from '@/components/landing/LiveMatchesBanner';
 import { FinishedMatches } from '@/components/landing/FinishedMatches';
-import Navbar from '@/components/layout/navbar';
+import { Header } from '@/components/layout/header';
 import Hero from '@/components/layout/hero';
 import { SocialsBanner } from '@/components/landing/SocialsBanner';
 import { LatestNewsBanner } from '@/components/landing/LatestNewsBanner';
@@ -13,7 +13,7 @@ import { motion } from 'framer-motion';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <Navbar />
+      <Header />
       <Hero />
       <motion.main 
         className="flex-1"
