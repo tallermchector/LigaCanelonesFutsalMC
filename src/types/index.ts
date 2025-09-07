0d970183b2f84663431693d298eb38ba619a8133
Hector Larrea <taller.mcmoto@gmail.com>
2025-09-07 05:29:31 +0000

    [{ 	"resource": "/home/user/studio/src/actions/team-actions.ts", 	"owner

--- src/types/index.ts
+++ src/types/index.ts
@@ -35,7 +35,6 @@ export interface GameEvent extends Omit<PrismaGameEvent, 'type'> {
 }
 
 export interface FullMatch extends Omit<PrismaMatch, 'teamAId' | 'teamBId' | 'events' | 'scheduledTime' | 'status'> {
-  [x: string]: number;
   scheduledTime: string; 
   status: MatchStatus;
   teamA: Team;
