16cf486c9713f7e1745d3e3666b5a542857bfd0d
Hector Larrea <taller.mcmoto@gmail.com>
2025-09-09 05:23:47 +0000

    # Paso 1: Instalar y Configurar 'next-pwa' para Funcionalidades PWA
    **O

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,13 @@
 import type {NextConfig} from 'next';
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -56,4 +65,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
